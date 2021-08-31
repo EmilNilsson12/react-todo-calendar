e12d5975ec3d7412b9c075911832156c944e1c30
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-08-31 11:03:03 +0200

    Add navigation between months and years

--- src/Components/CalenderView/CalenderView.js
+++ src/Components/CalenderView/CalenderView.js
@@ -1,5 +1,43 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
 function CalenderView() {
-	return <div>View of current month</div>;
+	const [momentObj, setMomentObject] = useState(() => moment());
+	const [currentViewMonth, setCurrentViewMonth] = useState();
+	const [currentViewYear, setCurrentViewYear] = useState();
+
+	useEffect(() => {
+		setCurrentViewMonth(momentObj.format('MMMM'));
+		setCurrentViewYear(momentObj.format('YYYY'));
+	}, [momentObj]);
+
+	const prevMonth = () => {
+		console.log('Show prev month');
+		momentObj.subtract(1, 'M');
+		setCurrentViewMonth(momentObj.format('MMMM'));
+		setCurrentViewYear(momentObj.format('YYYY'));
+	};
+
+	const nextMonth = () => {
+		console.log('Show next month');
+		momentObj.add(1, 'M');
+		setCurrentViewMonth(momentObj.format('MMMM'));
+		setCurrentViewYear(momentObj.format('YYYY'));
+	};
+
+	useEffect(() => {
+		console.log('Calender now visible');
+	}, []);
+
+	return (
+		<div>
+			View of current month{' '}
+			<i>
+				{currentViewMonth} - {currentViewYear}
+			</i>
+			<button onClick={prevMonth}>Prev month</button>
+			<button onClick={nextMonth}>Next month</button>
+		</div>
+	);
 }
 
 export default CalenderView;
