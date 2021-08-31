ee0af5f8d1f47fca041dc76464a66d30f8a71f52
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-08-31 11:19:18 +0200

    Render a day per day in currentViewMonth

--- src/Components/CalenderView/CalenderView.js
+++ src/Components/CalenderView/CalenderView.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react';
 import moment from 'moment';
-function CalenderView() {
+function CalenderView({ todos }) {
 	const [momentObj, setMomentObject] = useState(() => moment());
 	const [currentViewMonth, setCurrentViewMonth] = useState();
 	const [currentViewYear, setCurrentViewYear] = useState();
+	const [daysInThisMonth, setDaysInThisMonth] = useState();
 
 	const updateStates = () => {
 		setCurrentViewMonth(momentObj.format('MMMM'));
 		setCurrentViewYear(momentObj.format('YYYY'));
+		setDaysInThisMonth(momentObj.daysInMonth());
 	};
 
+	// Only fired once since momentObj is only ever mutated, never re-assigned
+	// Basically componentDidMount
 	useEffect(() => {
 		updateStates();
 	}, [momentObj]);
@@ -24,12 +28,24 @@ function CalenderView() {
 		updateStates();
 	};
 
+	const renderDays = () => {
+		const components = [];
+
+		for (let i = 1; i <= daysInThisMonth; i++) {
+			components.push(<span>__-day {i}--_</span>);
+		}
+		return components;
+	};
 	return (
 		<div>
 			View of current month{' '}
 			<i>
 				{currentViewMonth} - {currentViewYear}
 			</i>
+			<div>
+				{currentViewMonth} has {daysInThisMonth} days
+			</div>
+			<div>{renderDays()}</div>
 			<button onClick={prevMonth}>Prev month</button>
 			<button onClick={nextMonth}>Next month</button>
 		</div>
