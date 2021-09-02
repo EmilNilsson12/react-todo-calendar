5d24b8e0f309eb314137a4ab871209b6561ba2a0
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-02 15:22:48 +0200

    Convert inputs to controlled components

--- src/App.js
+++ src/App.js
@@ -8,25 +8,42 @@ import GetAllTodos from './FetchRequests/GetAllTodos';
 function App() {
 	const [showCalender, toggleShowCalender] = useState(false);
 
-	const [fetchingTodos, setFetchingTodos] = useState(true);
-	const [todos, setTodos] = useState();
+	// const [fetchingTodos, setFetchingTodos] = useState(true);
+	const [todos, setTodos] = useState(
+		JSON.parse(localStorage.getItem('todos')) || []
+	);
 
-	const fetchAndSaveTodos = () => {
-		console.log('fetchAndSaveTodos is called!');
-		setTodos(
-			GetAllTodos((e) => {
-				console.log('todos parsed: ', e);
-				setTodos(e);
-				setFetchingTodos(false);
-			})
-		);
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
+	const crudOperations = {
+		addTodo: ({ inputTitle, inputDescription, inputDate }) => {
+			console.log('Add todo');
+			console.log('Add inputTitle: ', inputTitle);
+			console.log('Add inputDescription: ', inputDescription);
+			console.log('Add inputDate: ', inputDate);
+		},
+		deleteTodo: () => console.log('Delete todo'),
+		updateTodo: () => console.log('Update todo'),
 	};
 
-	// componentDidMount
-	useEffect(() => {
-		console.log('App "componentDidMount"');
-		fetchAndSaveTodos();
-	}, []);
+	// const fetchAndSaveTodos = () => {
+	// 	console.log('fetchAndSaveTodos is called!');
+	// 	setTodos(
+	// 		GetAllTodos((e) => {
+	// 			console.log('todos parsed: ', e);
+	// 			setTodos(e);
+	// 			setFetchingTodos(false);
+	// 		})
+	// 	);
+	// };
+
+	// // componentDidMount
+	// useEffect(() => {
+	// 	console.log('App "componentDidMount"');
+	// 	fetchAndSaveTodos();
+	// }, []);
 
 	const callBacks = {
 		swapView: () => {
@@ -37,11 +54,9 @@ function App() {
 		<div className='App'>
 			<SwapView cbFunc={callBacks.swapView} />
 			{showCalender ? (
-				<CalenderView todos={todos} />
-			) : fetchingTodos ? (
-				<div>Loading...</div>
+				<CalenderView todos={todos} crudOperations={crudOperations} />
 			) : (
-				<ListTodosView todos={todos} />
+				<ListTodosView todos={todos} crudOperations={crudOperations} />
 			)}
 		</div>
 	);
--- src/Components/TodoForm/TodoForm.js
+++ src/Components/TodoForm/TodoForm.js
@@ -1,16 +1,33 @@
 import moment from 'moment';
 import { useState } from 'react';
 import './TodoForm.css';
-function TodoForm({ defaultDate }) {
+function TodoForm({ defaultDate, addTodo }) {
 	const handleSubmit = (evt) => {
 		evt.preventDefault();
 		console.log('Success');
+		addTodo({
+			inputTitle,
+			inputDescription,
+			inputDate,
+		});
+		setInputTitle('');
+		setInputDescription('');
 	};
 
+	const [inputTitle, setInputTitle] = useState('');
+	const [inputDescription, setInputDescription] = useState('');
 	const [inputDate, setInputDate] = useState(defaultDate);
 
-	const handleDateChange = (evt) => {
-		let dateComponent = evt.target.value;
+	const handleTitleChange = ({ target }) => {
+		setInputTitle(target.value);
+	};
+
+	const handleDescriptionChange = ({ target }) => {
+		setInputDescription(target.value);
+	};
+
+	const handleDateChange = ({ target }) => {
+		let dateComponent = target.value;
 		let timeComponent = defaultDate.toISOString().split('T')[1];
 		let newDate = moment(dateComponent + 'T' + timeComponent);
 
@@ -23,14 +40,23 @@ function TodoForm({ defaultDate }) {
 			<form onSubmit={handleSubmit}>
 				<label>
 					Title
-					<input type='text' required />
+					<input
+						type='text'
+						value={inputTitle}
+						onChange={handleTitleChange}
+						required
+					/>
 				</label>
 				<label>
 					Additional info
-					<input type='text' />
+					<input
+						type='text'
+						value={inputDescription}
+						onChange={handleDescriptionChange}
+					/>
 				</label>
 				<label>
-					Deadline: <b>{moment(inputDate).add('h', 8).fromNow()}</b>
+					Deadline: <b>{moment(inputDate).add(8, 'h').fromNow()}</b>
 					<input
 						type='date'
 						value={inputDate.toISOString().split('T')[0]}
