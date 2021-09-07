3f01e9088b2c8642c7e2ef00e5ab9d2821972919
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-07 13:19:28 +0200

    Remove unused code

--- src/Components/CalenderView/DayOfMonth/DayOfMonth.js
+++ src/Components/CalenderView/DayOfMonth/DayOfMonth.js
@@ -1,22 +1,6 @@
 function DayOfMonth({ day, placeHolder, today, active, cbFunc, hasTodos }) {
 	const generateNotches = () => {
-		// let max = hasTodos;
-		// if (max > 2) max = 2;
-
 		let innerText = `This day has ${hasTodos} todo${hasTodos > 1 ? 's' : ''}`;
-		// const container = [];
-		// for (let i = 0; i < max; i++) {
-		// 	console.log(hasTodos);
-		// 	container.push(
-		// 		<span key={i} className='notch'>
-		// 			|{' '}
-		// 		</span>
-		// 	);
-		// }
-		// console.log('hasTodos: ', hasTodos);
-		// if (hasTodos > 2) {
-		// 	container.push(<span className='notch'>...</span>);
-		// }
 		return innerText;
 	};
 	return (
